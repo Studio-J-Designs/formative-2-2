b118c740fcdd9e5c5e50ab18f58eb1cf92de2052
Studio J Designs <53199757+Studio-J-Designs@users.noreply.github.com>
2019-11-07 15:30:32 +1300

    Initial commit

---
+++ styles/js/master.js
@@ -0,0 +1,30 @@
+/*
+
+// Function to clear printed table
+function clear(){
+    document.getElementById('registryResults').innerHTML = '';
+}
+
+// Creating table heading
+function tableHeading(){
+    document.getElementById('registryResults').innerHTML +=
+    '<thead class="thead-dark">' +
+        '<tr>' +
+            '<th>Name</th>' +
+            '<th>Age</th>' +
+            '<th>Weapon</th>' +
+        '</tr>' +
+    '</thead>';
+}
+
+// Function that writes appropriate information to results table
+function writeFencers(){
+    document.getElementById('registryResults').innerHTML +=
+    '<tr>' +
+        '<td>' + fencer[i].lastName + ', ' + fencer[i].firstName + '</td>' +
+        '<td>' + fencer[i].age + '</td>' +
+        '<td>' + fencer[i].weapon[0] + '</td>' +
+    '</tr>';
+}
+
+*/
